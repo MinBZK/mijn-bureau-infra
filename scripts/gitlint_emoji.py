05cb42b910f45b642b5f10bcea85a609cac2ab12
Luc Haaijer <luc@haaijer.dev>
2026-03-02 20:07:18 +0000

    ✅ (tests) tests for gateway API support
    Maybe a bit too much for one commit.
    
    Follow ingress.enabled to allow disabling ingress for specific charts.
    Support cli-style gitmoji
    Fix race condition in testing script
    Formatting

--- scripts/gitlint_emoji.py
+++ scripts/gitlint_emoji.py
@@ -32,12 +32,12 @@ class GitmojiTitle(LineRule):
         ).json()["gitmojis"]
         emojis = [item["emoji"] for item in gitmojis]
 
-        pattern = r"^({:s})\(.*\)\s[a-zA-Z].*$".format("|".join(emojis))
+        pattern = r"^({:s})(\s?\(.*\))?\s[a-zA-Z].*$".format("|".join(emojis))
         if not re.search(pattern, title):
-            violation_msg = 'Title does not match regex "<gitmoji>(<scope>) <subject>"'
+            violation_msg = 'Title does not match regex "<gitmoji> [(<scope>)] <subject>"'
 
             # Special case for deps, updatecli forces a : after deps
-            depspattern = r"^({:s})\(deps\):\s[a-zA-Z].*$".format("|".join(emojis))
+            depspattern = r"^({:s})\s?\(deps\):\s[a-zA-Z].*$".format("|".join(emojis))
             if not re.search(depspattern, title):
               return [RuleViolation(self.id, violation_msg, title)]
 
@@ -53,7 +53,7 @@ class GitmojiTitle(LineRule):
         scopes.append("policies")
         scopes.append("ci")
         scopes.append("other")
-        pattern = r"^({:s})\({:s}\)\s[a-zA-Z].*$".format("|".join(emojis), "|".join(scopes))
+        pattern = r"^({:s})(\s?\({:s}\))?\s[a-zA-Z].*$".format("|".join(emojis), "|".join(scopes))
         if not re.search(pattern, title):
             violation_msg = 'The scope should be one of: {:s}'.format(", ".join(scopes))
             return [RuleViolation(self.id, violation_msg, title)]
